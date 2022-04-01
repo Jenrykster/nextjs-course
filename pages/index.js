c5997ab771d20254a705f3c1b45ae350bbbeb48f
João Henrique <joaoenrique.san@gmail.com>
2022-04-01 17:52:36 -0300

    feat: finish module 5

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import fs from 'fs/promises';
+import Link from 'next/link';
 import path from 'path';
 
 export default function HomePage(props) {
@@ -7,7 +8,9 @@ export default function HomePage(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
@@ -18,9 +21,20 @@ export async function getStaticProps() {
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
