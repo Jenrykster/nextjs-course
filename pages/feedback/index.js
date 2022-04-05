c7c2a43fb48b13a226e76f23f3040bfd1e69c703
João Henrique <joaoenrique.san@gmail.com>
2022-04-05 15:13:51 -0300

    feat: finish module 8

---
+++ pages/feedback/index.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
+import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+
+function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+  return (
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}{' '}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+  console.log(data);
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
