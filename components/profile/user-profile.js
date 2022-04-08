e105311031caf3e9db641c68f315fe0c43ff4a57
João Henrique <joaoenrique.san@gmail.com>
2022-04-08 18:38:36 -0300

    feat: finish module 13

---
+++ components/profile/user-profile.js
@@ -0,0 +1,41 @@
+import { session, useSession, getSession } from 'next-auth/client';
+import { useEffect, useState } from 'react';
+import ProfileForm from './profile-form';
+import classes from './user-profile.module.css';
+
+function UserProfile() {
+  // Redirect away if NOT auth
+  /* const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = '/auth';
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  } */
+
+  function changePasswordHandler(passwordData) {
+    const response = await fetch('api/user/change-password', {
+      method: 'PATCH',
+      body: JSON.stringify(passwordData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+  return (
+    <section className={classes.profile}>
+      <h1>Your User Profile</h1>
+      <ProfileForm onChangePassword={changePasswordHandler} />
+    </section>
+  );
+}
+
+export default UserProfile;
