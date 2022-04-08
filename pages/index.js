3f75714409f03d220ba2e20b5d49ab093d3d9db5
João Henrique <joaoenrique.san@gmail.com>
2022-04-08 11:40:35 -0300

    fix: import casing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Fragment } from 'react';
 import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/Hero';
+import Hero from '../components/home-page/hero';
 import { getFeaturedPosts } from '../lib/posts-util';
 import Head from 'next/head';
 
